7b493f7c18c16122adcee38596271554b74ff44b
wachira <wachiraa954@gmail.com>
2022-06-18 17:49:17 +0300

    assign domcontentloadedb done

--- test/indexTest.js
+++ test/indexTest.js
@@ -2,7 +2,7 @@ describe( "index.js", () => {
   it( 'should change the text on the page when DOM content has loaded', function () {
     expect( document.querySelector( 'p' )
         .textContent )
-      .to.equal( "JavaScript is so cool. It lets me add text to my page programmatically." );
+    .to.equal( "JavaScript is so cool. It lets me add text to my page programmatically." );
 
     const event = document.createEvent( 'Event' );
     event.initEvent( 'DOMContentLoaded', true, true );
