9e5d5658d9db14dc3fc29411a206c7fc4a2e252e
wachira <wachiraa954@gmail.com>
2022-06-18 17:48:25 +0300

    commit tree 3be44161aa8cbd6cb8fd6e6c5243e5cff4588cd1

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('The DOM has loaded')
+    console.log(document.querySelector('p'))
+})
+console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+console.log('Before DOM Loads')
+console.log(document.querySelector('p'))
+
+
+
+const event= document.createEvent('Event')
+if (document.querySelector('p') === "This is really cool");
+console.log('This is really cool')
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('The DOM has loaded')
+    console.log(document.querySelector('p'))
+    const event= document.createEvent('Event')
+if (document.querySelector('p') === "This is really cool");
+console.log('This is really cool')
+})
